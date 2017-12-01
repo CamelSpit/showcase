bc5e2113fc9fd761389f968d8c21eff56f45c30b
Wheatley <Wheatley@MacBook-Pro.local>
2017-12-01 15:29:32 -0700

    this is painful

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import App from "../../App";
+
+class FilterObject extends Component {
+    render(){
+        return(
+            <p>FilterObject</p>
+        );
+    }
+}
+
+export default FilterObject;
